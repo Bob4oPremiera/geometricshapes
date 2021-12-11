7b3c84e738fd04bb1211ca28827f9c3b535a7153
Bob4oPremiera <79801773+Bob4oPremiera@users.noreply.github.com>
2021-12-11 11:31:04 +0200

    Add files via upload

---
+++ mainofgeometricshapes.py
@@ -0,0 +1,21 @@
+import turtle as t
+def square():
+    side = input()
+    for i in range(4):
+        t.forward(side)
+        t.right(90)
+def triangle():
+    side = input()
+    for i in range(3):
+        t.forward(side)
+        t.right(60)
+def rectangle():
+    print("Please write to console size sides (at pixels):")
+    side = input()
+    side_ = input()
+    t.forward(side)
+    t.right()
+
+t.hideturtle()
+t.done()
+t.screensize(100,100)
